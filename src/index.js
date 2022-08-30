a081ceebb995571da2c3d4057b19d1fb1a16ff2e
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-30 07:05:09 -0400

    testing prettier

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
-import ReactDOM from  'react-dom'
+import ReactDOM from "react-dom";
+import Saludo from "./testing";
 
 function Saludar() {
-  return <h1>mi primer componente de react</h1>
+  return (
+    <div>
+      <h1>mi primer componente de react</h1>
+      <Saludo />
+    </div>
+  );
 }
 
-ReactDOM.render(<Saludar />, document.getElementById('root'));
+ReactDOM.render(<Saludar />, document.getElementById("root"));
 
 // rafce crea un react function arrow
---
+++ src/testing.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Saludo = () => {
+  return (
+    <div>
+      <article>
+        <ul>
+          <li>
+            <a href="#">link example</a>
+          </li>
+        </ul>
+      </article>
+    </div>
+  );
+};
+
+export default Saludo;
