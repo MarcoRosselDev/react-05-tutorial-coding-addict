26fa19ed3e6ff5bd9e59f1e8d1f4c7cc7dd99c21
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-31 06:50:26 -0400

    component structure test

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ function Saludar() {
   return (
     <section>
       <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
     </section>
   );
 }
@@ -14,18 +18,21 @@ const Book = () => {
     <article>
       this is a cart
       <Imag />
+      <Title />
+      <Author />
     </article>
   );
 };
 
-const Imag = () => {
-  return (
-    <img
-      src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51MT0MbpD7L.jpg"
-      alt="book test url"
-    />
-  );
-};
+const Imag = () => (
+  <img
+    src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51MT0MbpD7L.jpg"
+    alt="book test url"
+  />
+);
+
+const Title = () => <h1>The subltle art of not giving a fuch</h1>;
+const Author = () => <h4>Roger Wayne</h4>;
 
 ReactDOM.render(<Saludar />, document.getElementById("root"));
 
