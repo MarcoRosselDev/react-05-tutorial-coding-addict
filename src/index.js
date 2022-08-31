decbd09ae65419d384a3474b7d5ccf63714e3dfa
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-31 09:00:21 -0400

    testing variable scope in react

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 
 // CSS
 import "./index.css";
+// setup vars
+const title = "Title: The subltle art of not giving a fuch";
+const author = "Author: Roger Wayne";
+const img =
+  "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51MT0MbpD7L.jpg";
 
 function Saludar() {
   return (
@@ -16,12 +21,9 @@ const Book = () => {
   return (
     <article className="book">
       <p>book</p>
-      <img
-        src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51MT0MbpD7L.jpg"
-        alt="book test url"
-      />
-      <h1>Title: The subltle art of not giving a fuch</h1>
-      <h4>Author: Roger Wayne</h4>
+      <img src={img} alt="book test url" />
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
     </article>
   );
 };
