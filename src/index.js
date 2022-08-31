52816294a3bc5d004f9d16fc97839d7b18bb1963
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-31 07:29:15 -0400

    finish testing component

--- src/index.js
+++ src/index.js
@@ -8,29 +8,6 @@ function Saludar() {
   return (
     <section className="booklist">
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
@@ -39,26 +16,15 @@ const Book = () => {
   return (
     <article className="book">
       <p>book</p>
-      <Imag />
-      <Title />
-      <Author />
+      <img
+        src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51MT0MbpD7L.jpg"
+        alt="book test url"
+      />
+      <h1>Title: The subltle art of not giving a fuch</h1>
+      <h4>Author: Roger Wayne</h4>
     </article>
   );
 };
-
-const Imag = () => (
-  <img
-    src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51MT0MbpD7L.jpg"
-    alt="book test url"
-  />
-);
-
-const Title = () => <h1>The subltle art of not giving a fuch</h1>;
-const Author = () => (
-  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-    Roger Wayne
-  </h4>
-);
 
 ReactDOM.render(<Saludar />, document.getElementById("root"));
 
