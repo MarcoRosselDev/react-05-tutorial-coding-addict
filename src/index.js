1ce913201445ec1fd79491a52ec5f4977e76ad92
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-31 06:43:47 -0400

    component into component

--- src/index.js
+++ src/index.js
@@ -5,17 +5,26 @@ function Saludar() {
   return (
     <section>
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
 
 const Book = () => {
-  return <article>this is a cart</article>;
+  return (
+    <article>
+      this is a cart
+      <Imag />
+    </article>
+  );
+};
+
+const Imag = () => {
+  return (
+    <img
+      src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51MT0MbpD7L.jpg"
+      alt="book test url"
+    />
+  );
 };
 
 ReactDOM.render(<Saludar />, document.getElementById("root"));
