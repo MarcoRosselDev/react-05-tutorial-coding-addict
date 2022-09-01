0a90bcb49fd56d8c07af674c3950b0dc8c98e12b
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-01 12:45:05 -0400

    unique id solved

--- src/index.js
+++ src/index.js
@@ -7,14 +7,22 @@ import "./index.css";
 // object books
 const books = [
   {
+    id: 1,
     img: "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51MT0MbpD7L.jpg",
     title: "Title: The subltle art of not giving a fuch",
     author: "Author: Roger Wayne",
   },
   {
+    id: 2,
     img: "https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg",
-    title: "I Love You to the Moon and BackS",
+    title: "I Love You to the Moon and Back",
     author: "Tim Warnes",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/41KY-NORo9L._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "The 48 Laws of Power Paperback",
+    author: "Robert Greene",
   },
 ];
 
@@ -22,29 +30,16 @@ function Booklist() {
   return (
     <section className="booklist">
       {books.map((book) => {
-        console.log(book);
-        return "wsop";
+        return <Book key={book.id} book={book}></Book>;
       })}
     </section>
   );
 }
-/*
-function Saludar() {
-  return (
-    <section className="booklist">
-      <Book job="developer" />
-      <Book title="random title" number={29} />
-    </section>
-  );
-}*/
-
 // los dos son lo mismo
 const Book = (props) => {
-  const { img, title, author } = props; // para no repetir props.img props.title
-  // este requiere el props en parametros
+  const { img, title, author } = props.book;
   return (
     <article className="book">
-      <p>book</p>
       <img src={img} alt="book test url" />
       <h1>{title}</h1>
       <h4>{author.toUpperCase()}</h4>
