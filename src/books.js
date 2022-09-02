ef7c58ab4f4008cb440dae6f6d88faa6d58c7399
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-02 19:25:45 -0400

    books.js import export

---
+++ src/books.js
@@ -0,0 +1,21 @@
+// object books
+export const books = [
+  {
+    id: 1,
+    img: "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51MT0MbpD7L.jpg",
+    title: "Title: The subltle art of not giving a fuch",
+    author: "Author: Roger Wayne",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg",
+    title: "I Love You to the Moon and Back",
+    author: "Tim Warnes",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/415tR+XH9NL._SX332_BO1,204,203,200_.jpg",
+    title: "The 48 Laws of Power Paperback",
+    author: "Robert Greene",
+  },
+];
